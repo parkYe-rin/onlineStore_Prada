88c4a879de56d857e071f79ae898744769929fd6
parkYe-rin <dogfoot123890@gmail.com>
2024-03-28 23:34:26 +0900

    corret url

--- src/page/ProductDetails.js
+++ src/page/ProductDetails.js
@@ -26,7 +26,7 @@ const ProductDetails = () => {
   };
 
   const pageDetails = async () => {
-    let url = `http://localhost:3004/parkYe-rin/onlineStore_Prada/products/${id}`;
+    let url = `http://my-json-server.typicode.com/parkYe-rin/onlineStore_Prada/products/${id}`;
     let res = await fetch(url);
     let data = await res.json();
     setDetails(data);
